2e3b0dc56443aa024876dbd3a444ea3c3a8978d0
YukkuriC <799433638@qq.com>
2024-10-30 23:17:57 +0800

    来都来了

---
+++ src/dev/latvian/mods/rhino/util/HideFromJS.java
@@ -0,0 +1,10 @@
+package dev.latvian.mods.rhino.util;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({})
+public @interface HideFromJS {
+}
