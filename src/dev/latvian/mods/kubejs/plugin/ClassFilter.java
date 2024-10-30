af221df2a29b99f15766665943019a692eb95b2e
YukkuriC <799433638@qq.com>
2024-10-30 23:20:05 +0800

    various fixes

--- src/dev/latvian/mods/kubejs/plugin/ClassFilter.java
+++ src/dev/latvian/mods/kubejs/plugin/ClassFilter.java
@@ -1,12 +1,6 @@
 package dev.latvian.mods.kubejs.plugin;
 
 import dev.latvian.mods.kubejs.script.ScriptType;
-import it.unimi.dsi.fastutil.objects.Object2ByteOpenHashMap;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class ClassFilter {
     public final ScriptType scriptType;
